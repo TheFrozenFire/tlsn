16ac8459a1ef6b49802d884652f49f9b9c29a17b
Justin Martin <justin@keyring.network>
2025-04-30 14:56:13 -0700

    JSON context checker

--- crates/formats/src/json/context.rs
+++ crates/formats/src/json/context.rs
@@ -1,4 +1,6 @@
-use spansy::json::JsonValue;
+use spansy::json::{JsonValue, JsonVisit};
+use spansy::json as types;
+use spansy::Spanned;
 
 /// A verifier of contextual integrity for JSON.
 ///
@@ -11,30 +13,81 @@ pub struct JsonContext {
 impl JsonContext {
     /// Creates a new builder.
     pub fn builder(
-        value: JsonValue,
         structure: JsonValue,
+        value: JsonValue,
     ) -> JsonContextBuilder {
-        JsonContextBuilder::new(value, structure)
+        JsonContextBuilder::new(structure, value)
     }
 }
 
 /// Builder for [`JsonContext`].
 pub struct JsonContextBuilder {
-    value: JsonValue,
     structure: JsonValue,
+    value: JsonValue,
 }
 
 impl JsonContextBuilder {
     /// Creates a new builder.
     pub fn new(
-        value: JsonValue,
         structure: JsonValue,
+        value: JsonValue,
     ) -> Self {
-        Self { value, structure }
+        Self { structure, value }
     }
 
     /// Builds the context.
     pub fn build(self) -> Result<JsonContext, Box<dyn std::error::Error>> {
+        DefaultJsonContextVisitor::default().visit_value(&self.structure, &self.value);
+
         Ok(JsonContext { value: self.value })
     }
 }
+
+/// A visitor for JSON values that checks for structural integrity.
+pub trait JsonContextVisitor {
+    /// Visit a JSON value.
+    fn visit_value(&mut self, structure: &JsonValue, value: &JsonValue) {
+        // Ensure value has same variant as structure
+        match (structure, value) {
+            (JsonValue::Null(_), JsonValue::Null(_)) => (),
+            (JsonValue::Redacted(_), _) => (),
+
+            (JsonValue::Bool(structure), JsonValue::Bool(value)) => assert_eq!(structure.span().as_str(), value.span().as_str()),
+            (JsonValue::Number(structure), JsonValue::Number(value)) => assert_eq!(structure.span().as_str(), value.span().as_str()),
+            (JsonValue::String(structure), JsonValue::String(value)) => assert_eq!(structure.span().as_str(), value.span().as_str()),
+
+
+            (JsonValue::Array(structure), JsonValue::Array(value)) => self.visit_array(structure, value),
+            (JsonValue::Object(structure), JsonValue::Object(value)) => self.visit_object(structure, value),
+
+            _ => panic!("Type mismatch: expected {:?}, got {:?}", structure, value)
+        }
+    }
+
+    /// Visit a JSON object.
+    fn visit_object(&mut self, structure: &types::Object, value: &types::Object) {
+        for elem in structure.elems.iter() {
+            let matching = value.elems.iter().find(|e| e.key.span().as_str() == elem.key.span().as_str());
+            assert!(matching.is_some(), "Missing key: {}", elem.key.span().as_str());
+
+            self.visit_value(&elem.value, &matching.unwrap().value);
+        }
+    }
+
+    /// Visit a JSON array.
+    /// The default strategy enforces that if the array is non-empty, then the
+    /// value must be an array with the same number of elements, and that each
+    /// non-redacted element is structurally equivalent to the corresponding
+    /// element in the structure.
+    fn visit_array(&mut self, structure: &types::Array, value: &types::Array) {
+        for (i, elem) in structure.elems.iter().enumerate() {
+            self.visit_value(elem, &value.elems[i]);
+        }
+    }
+}
+
+/// The default JSON context visitor.
+#[derive(Debug, Default)]
+pub struct DefaultJsonContextVisitor {}
+
+impl JsonContextVisitor for DefaultJsonContextVisitor { }
--- crates/formats/src/json/mod.rs
+++ crates/formats/src/json/mod.rs
@@ -5,7 +5,7 @@ mod context;
 use spansy::json;
 
 pub use commit::{DefaultJsonCommitter, JsonCommit, JsonCommitError};
-pub use context::{JsonContext, JsonContextBuilder};
+pub use context::{DefaultJsonContextVisitor, JsonContext, JsonContextBuilder, JsonContextVisitor};
 pub use json::{
     Array, Bool, JsonKey, JsonValue, JsonVisit, KeyValue, Null, Number, Object, String,
 };
